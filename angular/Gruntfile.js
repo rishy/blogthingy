f134db7fb9a9a4387ba038a5380157309dd5a68d
rishy <rishy.s13@gmail.com>
2014-05-04 02:53:37 +0530

    removed unused copying of files in assets folder

--- angular/Gruntfile.js
+++ angular/Gruntfile.js
@@ -309,8 +309,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
-            '.htaccess',
-            '*.html',
+            '.htaccess',            
             'views/{,*/}*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
@@ -325,7 +324,7 @@ module.exports = function (grunt) {
           expand: true,
           cwd: '.tmp/',
           dest: '<%= yeoman.dist %>',
-          src: ['{,*/}*.js', '{,*/}*.css', '{,*/}*.html']
+          src: ['{,*/}*.js', '{,*/}*.css', 'views/{,*/}*.html']
         }]
       },
       styles: {
